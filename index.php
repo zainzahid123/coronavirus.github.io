f2ed09628759ce978017ab0d89c3ff122488f4db
zainzahid123 <63954533+zainzahid123@users.noreply.github.com>
2020-04-19 15:53:46 +0500

    corona website

---
+++ index.php
@@ -0,0 +1,327 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Against corona</title>
+    <meta charset="UTF-8">
+    <title>Portfolio</title>
+
+    <!--bootstrap css framework library linked-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Muli" />
+    <!--custom css linked-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--animate-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <!--jquery library linked framework of javascript-->
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg nav_style p-3">
+    <a class="navbar-brand pl-5 font-weight-bolder" href="index.php">COVID-19</a>
+    <button class="navbar-toggler navbar-dark bg-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto pr-5 text-capitalize">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">about</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#symptoms">symptoms</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#prevention">prevention</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="main_header">
+    <div class="row w-100 h-100">
+        <div class="col-lg-5 col-md-5 col-12 order-lg-1 order-2">
+            <div class="left_side w-100 h-100 d-flex justify-content-center align-items-center">
+                <img src="images/novel.png" class="logo"  alt="">
+            </div>
+        </div>
+        <div class="col-lg-7 col-md-7 col-12 order-lg-2 order-1">
+            <div class="right_side  w-100 h-100 d-flex justify-content-center align-items-center">
+                <h1>Let's Stay Safe & Fight Against Cor<span class="corona_rot"><img src="images/O.png" alt=""></span>na</h1>
+            </div>
+        </div>
+    </div>
+</div>
+                            <!---corona latest update*****************************8-->
+<section class="corona_update scrolly">
+    <div class="mb-3">
+        <h1 class="text-center">Covid-19 Updates</h1>
+    </div>
+    <div class="d-flex justify-content-around align-items-center count_style">
+        <div>
+            <h2 class="count">886,108</h2>
+            <p>Cases reported in whole world</p>
+        </div>
+        <div>
+            <h2 class="count">2,039</h2>
+            <p>Cases reported in Pakistan</p>
+        </div>
+        <div>
+            <h2 class="count">40</h2>
+            <p>Patients recovered In Pakistan</p>
+        </div>
+        <div>
+            <h2 class="count">26</h2>
+            <p>Deaths Reported in Pakistan</p>
+        </div>
+    </div>
+</section>
+<div class="container">
+    <div class="text-center">
+        <a href="https://www.worldometers.info/coronavirus/"><button class="btn btn-success bton mb-5">Present Situation</button></a>
+    </div>
+</div>
+<!--*******************************ABOUT SECTION**********************-->
+<div class="container-fluid sub_section pt-5 pb-5 scrolly" id="about">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1>About Covid-19</h1>
+    </div>
+    <div class="row pt-5">
+        <div class="col-lg-4 col-md-4 col-12">
+            <figure class="text-center">
+                <img src="images/about.jpg" width="400"  class="img-fluid mt-3" alt="">
+            </figure>
+        </div>
+        <div class="col-lg-8 col-md-8 col-12">
+            <h2>What is Covid-19(Corona Virus)</h2>
+            <p>Coronaviruses are a large family of viruses that can cause illness ranging from the common cold to
+                more severe diseases like Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS).
+                The 2019 novel coronavirus, called 'SARS-CoV-2' (previously referred to as 2019-nCoV),
+                is a new strain that has not been identified in humans before. The disease that is caused by SARS-CoV-2 is called
+                'COVID-19'.</p>
+            <p>Generally, older people and those with underlying conditions (such as hypertension, heart disorders,
+                diabetes, liver disorders, and respiratory disease)
+                are expected to be more at risk of developing severe symptoms.
+                The evidence from analyses of cases to date is that COVID-19 infection causes mild disease
+                (i.e. non-pneumonia or mild pneumonia) in about 80% of cases and most cases recover; 14% have more severe
+                disease and 6% experience critical illness.</p>
+        </div>
+
+    </div>
+
+</div>
+<!--------corona Symptoms----------------------------------->
+<div class="container-fluid  pt-5 pb-5 scrolly" id="symptoms">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1>Corona Virus Symptoms</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <figure class="text-center">
+                    <img src="images/cough.jpg" class="img-fluid" width="120" height="120" alt="">
+                    <figcaption >
+                        Cough
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <figure class="text-center">
+                    <img src="images/runny-nose.png" class="img-fluid" width="120" height="120" alt="">
+                    <figcaption >
+                        Running Nose
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <figure class="text-center">
+                    <img src="images/illustration-child-diseases_29937-550.jpg" class="img-fluid" width="120" height="120" alt="">
+                    <figcaption >
+                        Fever
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <figure class="text-center">
+                    <img src="images/cold.jpeg" class="img-fluid" width="120" height="120" alt="">
+                    <figcaption >
+                        Cold
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <figure class="text-center">
+                    <img src="images/tired.jpg" class="img-fluid" width="120" height="120" alt="">
+                    <figcaption >
+                        tiredness
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <figure class="text-center">
+                    <img src="images/difficult.jpg" class="img-fluid" width="120" height="120" alt="">
+                    <figcaption >
+                        Difficulty in Breathing
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--------prevention Symptoms----------------------------------->
+<div class="container-fluid sub_section pt-5 pb-5 scrolly" id="prevention">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1>6 Steps Prevention Against Corona</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <figure class="text-center">
+                            <img src="images/wash.jpg" class="img-fluid" width="90" height="90" alt="">
+                        </figure>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-12 mt-3">
+                        <p>Wash your hands with soap regularly after come from outside.wash your hands in every 20 mints. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <figure class="text-center">
+                            <img src="images/maks.jpg" class="img-fluid" width="90" height="90" alt="">
+                        </figure>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-12 mt-3">
+                        <p>Always wear a good face mask before going outside to prevent from corona. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <figure class="text-center">
+                            <img src="images/close-contacts.jpg" class="img-fluid" width="90" height="90" alt="">
+                        </figure>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-12 mt-3">
+                        <p>Avoid from gathering. Avoid to go in public places. Avoid to meet an irrelevant person. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <figure class="text-center">
+                            <img src="images/home.jpg" class="img-fluid" width="90" height="90" alt="">
+                        </figure>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-12 mt-3">
+                        <p>Please stay at home don't go outside without any important reason. Cooperate with Government   </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <figure class="text-center">
+                            <img src="images/images.png" class="img-fluid" width="90" height="100" alt="">
+                        </figure>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-12 mt-3">
+                        <p>Stay informed through news channels about COVID-19 from news channels or from any other trustable resource. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 mt-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <figure class="text-center">
+                            <img src="images/med.jpg" class="img-fluid mt-4" width="90" height="90" alt="">
+                        </figure>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-12 mt-3">
+                        <p>Seek medical assistance if you fell that you are the victim of Corona virus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--*******************************CONTACT US**********************-->
+<div class="container-fluid pt-5 pb-5 scr" id="contact">
+    <div class="section_header text-center mb-5 mt-4">
+        <h1>Contact US In Case of Emergency</h1>
+    </div>
+    <div class="container ">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-12">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label>Your Name</label>
+                        <input type="text" class="form-control" name="username" placeholder="your name"
+                               autocomplete="off" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Email address</label>
+                        <input type="email" class="form-control" name="email" placeholder="name@example.com" required autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label>Mobile Number</label>
+                        <input type="number" class="form-control" name="mobile"  placeholder="Mobile Number "
+                               autocomplete="off" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Your Problem </label>
+                        <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-success">Submit</button>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+<footer class="mt-5 footer_style">
+    <div class="container-fluid text-white text-center ">
+        <p>Copy right by Zain Zahid.</p>
+        <p>Feel free to contact Us on <a href="https://mail.google.com/mail/u/1/#inbox">zainzainn651@gmail.com</a></p>
+    </div>
+</footer>
+
+
+<!-----jquery plugins------>
+<script src="js/jquery-3.4.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js" integrity="sha256-jDnOKIOq2KNsQZTcBTEnsp76FnfMEttF6AV2DF2fFNE=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js" integrity="sha256-JtQPj/3xub8oapVMaIijPNoM0DHoAtgh/gwFYuN5rik=" crossorigin="anonymous"></script>
+<script src="js/bootstrap.js"></script>
+<!--custom java script linked-->
+<script src="js/script.js"></script>
+</body>
+</html>
+
+<?php
+include 'links/conection.php';
+if(isset($_POST['submit'])){
+    $username= $_POST['username'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mobile'];
+    $message=$_POST['message'];
+
+$insertquery= "insert into  coronacases(username,email,mobile,message) value ('$username','$email','$mobile','$message')";
+$quey= mysqli_query($con,$insertquery);
+}
+
---
+++ links/conection.php
@@ -0,0 +1,16 @@
+<?php
+
+$con=mysqli_connect('localhost','root','','coronadb');
+/*if($con){
+    ?>
+    <script>
+        alert("connection succeful")
+    </script>
+    <?php
+}else{
+    ?>
+    <script>
+        alert("no connection")
+    </script>
+    <?php
+}*/
